cd78d3f413ab4dd65df1a05c36cdfae40530132a
donnyccp <onifer.campos@gmail.com>
2021-10-05 12:53:01 -0600

    Actualizacion implementacion tests

--- test/planilla_tests.cpp
+++ test/planilla_tests.cpp
@@ -15,20 +15,20 @@ namespace
         Planilla planilla;
         std::string linea="1 Alberta Parra alberta_parra@biz.com 1 ";
         std::string actual = planilla.calcularPlanillaPago(linea);       
-        EXPECT_EQ("1 Alberta Parra No Aplica 4278.911621 0 322.068604", actual);
+        EXPECT_EQ("1 Alberta Parra No Aplica 4278.911621 1 0 322.068604", actual);
      }
        
        TEST(PlanillaTests, Test_calcularPagoPorNomina) {
      
         Planilla planilla;        
         std::string actual = planilla.calcularPagoPorNomina(2, 1, "Jeannete", "Parra");
-        EXPECT_EQ("2 Jeannete Parra Denis Tyler 2880.675049 1 216.824997", actual);
+        EXPECT_EQ("2 Jeannete Parra Denis Tyler 2880.675049 2 1 216.824997", actual);
      }
 
        TEST(PlanillaTests, Test_calcularPagoPorHorasTrabajadas) {
      
         Planilla planilla;
         std::string actual = planilla.calcularPagoPorHorasTrabajadas(463, 22, "Fiorella", "Parra");
-        EXPECT_EQ("463 Fiorella Parra Jacob Carter 583.30004922 0.000000", actual);
+        EXPECT_EQ("463 Fiorella Parra Jacob Carter 583.300049 463 22 0.000000", actual);
      }
  }
